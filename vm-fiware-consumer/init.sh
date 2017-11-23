48e012a6ececba05bff2b67d89a788633366753b
jincera <jincera@users.noreply.github.com>
2017-11-22 19:40:55 -0600

    sudo wget
    Cambie sintaxis para instalar java

--- vm-fiware-consumer/init.sh
+++ vm-fiware-consumer/init.sh
@@ -52,14 +52,14 @@ sudo yum -y install psmisc
 ## Install Java 8
 cd /opt/
 
-BASE_URL_8=http://download.oracle.com/otn-pub/java/jdk/8u151-b12/e758a0de34e24606bca991d704f6dcbf/jdk-8u151
+BASE_URL_8="http://download.oracle.com/otn-pub/java/jdk/8u151-b12/e758a0de34e24606bca991d704f6dcbf/jdk-8u151"
 
 JDK_VERSION=`echo $BASE_URL_8 | rev | cut -d "/" -f1 | rev`
 
 declare -a PLATFORMS=("-linux-x64.tar.gz")
 
 
-wget -c --header "Cookie: oraclelicense=accept-securebackup-cookie" "${BASE_URL_8}${-linux-x64.tar.gz}"
+sudo wget -c --header "Cookie: oraclelicense=accept-securebackup-cookie" "$BASE_URL_8-linux-x64.tar.gz"
     ### curl -C - -L -O -# -H "Cookie: oraclelicense=accept-securebackup-cookie" "${BASE_URL_8}${platform}"
 
 sudo tar xzf jdk-8u151-linux-x64.tar.gz
